5c84d5bc17241691c714f027c117be6f51c74f4f
kousuke-amano <kousuke850850@gmail.com>
2021-02-11 17:05:51 +0900

    商品出品機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :state_id
-    validates :burden_id
-    validates :area_id
-    validates :scheduled_date_id
+    with_options numericality: { other_than: 1, message:"Select" } do
+      validates :category_id
+      validates :state_id
+      validates :burden_id
+      validates :area_id
+      validates :scheduled_date_id
+    end
     validates :price, format: { with: /\A[0-9]+\z/ }
     validates :price,
               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
@@ -21,4 +23,5 @@ class Item < ApplicationRecord
   #   validates :item_id, numericality: { other_than: 1 }
   #   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+  
 end
